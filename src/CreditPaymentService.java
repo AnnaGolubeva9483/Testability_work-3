9a4677589a6a8a25600fbc39352666c36420b03e
Anna Golubeva <agolubeva9483@gmail.com>
2023-04-02 22:30:58 +0300

    Payment

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,2 +1,10 @@
 public class CreditPaymentService {
+    public int calculate(double rateYear, int amountCredit, int termInMonth) {
+        double rateMonth = rateYear / 12 / 100;
+        double x = Math.pow((1 + rateMonth), termInMonth);
+        double coefficient = (rateMonth * x) / (x - 1);
+        double payment = amountCredit * coefficient;
+        return (int) payment;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
+
+        int paymentYear = service.calculate(9.99, 1_000_000, 12);
+        System.out.println((paymentYear) + " рублей");
+
+        int paymentTwoYears = service.calculate(9.99, 1_000_000, 24);
+        System.out.println((paymentTwoYears) + " рублей");
+
+        int paymentThreeYears = service.calculate(9.99, 1_000_000, 36);
+        System.out.println((paymentThreeYears) + " рублей");
+
+
     }
 }
